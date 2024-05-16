e2aa6791dd61f483f9340478afd39bb2a59c048f
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-05-16 22:26:17 +0530

    Updated

--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -5,7 +5,7 @@ import Button from "@elements/button";
 export default function Footer() {
   return (
     <div className="pb-6 border-t border-gray-200 bg-gray-50">
-      <div className="container mx-auto">
+      <div className="container mx-5 ">
         <div className="flex flex-wrap justify-center">
           <div className="flex justify-center w-full mb-8 sm:w-1/3 sm:justify-start sm:mb-0">
             <BrandIcon />
